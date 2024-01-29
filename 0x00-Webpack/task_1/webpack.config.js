9fe45ae92f06dac65045533bcf549321d4dadd17
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-29 19:15:17 +0100

    Add task_2 files

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
 
 module.exports = {
-  entry: './js/dashboard_main.js',
+  entry: {
+    index: path.resolve(__dirname, './js/dashboard_main.js')
+  },
   output: {
-    filename: '[main]bundle.js',
+    filename: '.bundle.js',
     path: path.resolve(__dirname, 'public')
   },
   mode: 'production'

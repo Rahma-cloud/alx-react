46e88f8c6130b1d8c2c9dbe0c29be4e569a74483
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-29 18:20:28 +0100

    Add task_2 files

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new OptimizeCssAssetsPlugin(),
+  ],
+};

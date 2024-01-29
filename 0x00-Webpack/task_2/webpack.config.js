35e7d4be83b0c2823eb9d5771bd2e418d3f8900b
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-29 20:19:27 +0100

    update tas2

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -17,6 +17,9 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+            filename: 'images/[name][ext][query]',
+        },
       },
     ],
   },

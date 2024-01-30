7ce2e8ff3868764f23fe45f04108f7ff7bed12b5
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-30 12:05:11 +0100

    Add task_1 files

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,8 @@
+// src/index.js
 import $ from 'jquery';
 
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<p>Copyright - Holberton School</p>');
+$(function() {
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+});

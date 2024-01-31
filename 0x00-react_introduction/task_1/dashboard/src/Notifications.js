87ac4de6391e3e0579b168f59d9d2ea8a2b3185c
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-01-31 10:28:49 +0100

    Add task_1 files and directories

---
+++ 0x00-react_introduction/task_1/dashboard/src/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Notifications from './Notification.css'
+
+function Notifications() {
+    return(
+        <div className="Notifications">
+            <p> Here is the list of notifications </p>
+        </div>
+    );
+}
+
+export default Notifications;

4e9d34d29d8d4edd2e010268c377bab58d32fa19
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-02-21 15:01:49 +0100

    Update task

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,14 +1,6 @@
+import { fromJS } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    let result = object;
-  
-    for (const key of array) {
-      if (result && (result instanceof Map || typeof result === 'object')) {
-        result = result[key];
-      } else {
-        result = undefined;
-        break;
-      }
-    }
-  
-    return result;
+  const immutableMap = fromJS(object);
+  return immutableMap.getIn(array, undefined);
 }

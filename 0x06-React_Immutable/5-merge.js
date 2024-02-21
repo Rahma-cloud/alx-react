54ef3122cfdc6ef1b14f3b75460c1d174194b0d8
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-02-21 17:39:39 +0100

    Update tasks

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -5,5 +5,5 @@ export default function concatElements(page1, page2) {
 }
 
 export default function mergeElements(page1, page2) {
-    return List(Map(page1).merge(page2));
+    return Map(page1).merge(Map(page2));
 }

a9839ece47d1a157bdfc3e635245cf93962e5109
Rahmat Folorunsho <folorunshorahmat16@gmail.com>
2024-02-21 14:55:51 +0100

    add task3 file

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,23 +1,14 @@
 export default function accessImmutableObject(object, array) {
     let result = object;
-
-  // Traverse the object based on the array of keys
-  for (const key of array) {
-    // Check if the current result is a Map or an object
-    if (result instanceof Map || typeof result === 'object') {
-      // Access the property using the current key
-      result = result[key];
-
-      // If the result is undefined, break the loop
-      if (result === undefined) {
+  
+    for (const key of array) {
+      if (result && (result instanceof Map || typeof result === 'object')) {
+        result = result[key];
+      } else {
+        result = undefined;
         break;
       }
-    } else {
-      // If the result is not a Map or an object, break the loop
-      break;
     }
-  }
-
-  // Return the final result (could be undefined, a string, or a Map)
-  return result;
+  
+    return result;
 }
